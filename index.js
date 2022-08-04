d575662e10292d1fc3ae91dfde8285b2d6bee3c2
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-08-04 10:14:47 -0400

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+class MyPlugin {
+  constructor() {
+    this.hooks = {
+      initialize: () => this.init(),
+      "before:deploy:deploy": () => this.beforeDeploy(),
+      "after:deploy:deploy": () => this.afterDeploy(),
+    };
+  }
+
+  init() {
+    console.log("Serverless instance: ", this.serverless);
+
+    // `serverless.service` contains the (resolved) serverless.yml config
+    const service = this.serverless.service;
+    console.log("Provider name: ", service.provider.name);
+    console.log("Functions: ", service.functions);
+  }
+
+  beforeDeploy() {
+    // Before deploy
+  }
+
+  afterDeploy() {
+    // After deploy
+  }
+}
+
+module.exports = MyPlugin;
