728f27cc7af1ae3743d788b96fa6902234e05144
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-08-05 10:05:10 -0400

    remove broken code

--- index.js
+++ index.js
@@ -12,10 +12,7 @@ class WafPlugin {
   init() {
     console.log("Serverless instance: ", this.serverless);
 
-    // `serverless.service` contains the (resolved) serverless.yml config
-    const service = this.serverless.service;
-    console.log("Provider name: ", service.provider.name);
-    console.log("Functions: ", service.functions);
+    console.log("hello world");
   }
 
   beforeDeploy() {
