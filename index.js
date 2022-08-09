a433783204590082cd37d0f717ab06c5941f4f5c
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-08-09 15:56:55 -0400

    working waf plugin - 1

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 "use strict";
 
 class WafPlugin {
-  constructor() {
+  constructor(serverless) {
+    this.serverless = serverless;
     this.hooks = {
       initialize: () => this.init(),
-      "before:deploy:deploy": () => this.beforeDeploy(),
-      "after:deploy:deploy": () => this.afterDeploy(),
+      "after:deploy:deploy": () => this.generateWafResource(),
+      "before:package:finalize": () => this.updateStack(),
     };
   }
 
   init() {
-    console.log("Serverless instance: ", this.serverless);
-    //add comment
-    console.log("hello world");
+    this.serverless.service.custom.testValue = "hello world";
+    console.log("serverless instance: ", this.serverless);
+    // this.generateWafResource();
   }
 
-  beforeDeploy() {
-    // Before deploy
+  generateWafResource() {
+    console.log("is this getting called");
+    // this.serverless.service.resources.Resources.APIGwWebAcl = {
+    //   Type: "AWS::WAFv2::WebACL",
+    //   Properties: {
+    //     Name: this.serverless.service.custom.webAclName + "hellothere",
+    //     DefaultAction: {
+    //       Block: {},
+    //     },
+    //   },
+    // };
+
+    // console.log(
+    //   "serverless instance resources: ",
+    //   this.serverless.service.resources
+    // );
+    // console.log(
+    //   "serverless instance resources properties: ",
+    //   this.serverless.service.resources.Resources.APIGwWebAcl.Properties
+    // );
   }
 
-  afterDeploy() {
-    // After deploy
+  updateStack() {
+    console.log(
+      "this is where the cloudformation stack will be updated for aws waf",
+      this.serverless.service.resources.Resources.APIGwWebAcl
+    );
+    this.serverless.service.provider.compiledCloudFormationTemplate.Resources.APIGwWebAclTest =
+      {
+        Type: "AWS::WAFv2::WebACL",
+        Properties: {
+          Name: this.serverless.service.custom.webAclName + "hellothere",
+          DefaultAction: {
+            Block: {},
+          },
+          VisibilityConfig: {
+            SampledRequestsEnabled: true,
+            CloudWatchMetricsEnabled: true,
+            MetricName: "testmetric",
+          },
+          Scope: "REGIONAL",
+        },
+      };
   }
 }
 
