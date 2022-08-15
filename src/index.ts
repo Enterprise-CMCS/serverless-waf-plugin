ef4fe750e7199d13e0de11c273626016f8135c4f
13bfrancis <40218571+13bfrancis@users.noreply.github.com>
2022-08-15 15:41:33 -0400

    fix(add-waf-rules): add waf rules

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 "use strict";
 
+import Serverless from "serverless";
+
 export class WafPlugin {
-  private serverless: any;
+  private serverless: Serverless;
   private hooks: any;
   constructor(serverless: any) {
     this.serverless = serverless;
@@ -23,23 +25,119 @@ export class WafPlugin {
   }
 
   updateStack() {
-    console.log(
-      "this is where the cloudformation stack will be updated for aws waf",
-      this.serverless.service.resources.Resources.APIGwWebAcl
-    );
+    // set the waf name based on stage and service name ${self:service}-${self:custom.stage}-webacl
+    const wafName = this.serverless.service.custom.wafName;
+    console.log("waf name: ", wafName);
+
     this.serverless.service.provider.compiledCloudFormationTemplate.Resources.APIGwWebAclTest =
       {
         Type: "AWS::WAFv2::WebACL",
         Properties: {
-          Name: this.serverless.service.custom.webAclName + "hellothere",
+          Name: wafName,
           DefaultAction: {
             Block: {},
           },
           VisibilityConfig: {
-            SampledRequestsEnabled: true,
             CloudWatchMetricsEnabled: true,
-            MetricName: "testmetric",
+            SampledRequestsEnabled: true,
+            MetricName: `${wafName}-webacl`,
           },
+          Rules: [
+            {
+              Name: `${wafName}-DDOSRateLimitRule`,
+              Priority: 0,
+              Action: {
+                Block: {},
+              },
+              Statement: {
+                RateBasedStatement: {
+                  Limit: 5000,
+                  AggregateKeyType: "IP",
+                },
+              },
+              VisibilityConfig: {
+                SampledRequestsEnabled: true,
+                CloudWatchMetricsEnabled: true,
+                MetricName: `${wafName}-DDOSRateLimitRuleMetric`,
+              },
+            },
+            {
+              Name: `${wafName}-AWSCommonRule`,
+              Priority: 1,
+              OverrideAction: {
+                None: {},
+              },
+              Statement: {
+                ManagedRuleGroupStatement: {
+                  VendorName: "AWS",
+                  Name: "AWSManagedRulesCommonRuleSet",
+                  ExcludedRules: [
+                    {
+                      Name: "SizeRestrictions_BODY",
+                    },
+                  ],
+                },
+              },
+              VisibilityConfig: {
+                SampledRequestsEnabled: true,
+                CloudWatchMetricsEnabled: true,
+                MetricName: `${wafName}-AWSCommonRuleMetric`,
+              },
+            },
+            {
+              Name: `${wafName}-AWSManagedRulesAmazonIpReputationList`,
+              Priority: 2,
+              OverrideAction: {
+                None: {},
+              },
+              Statement: {
+                ManagedRuleGroupStatement: {
+                  VendorName: "AWS",
+                  Name: "AWSManagedRulesAmazonIpReputationList",
+                },
+              },
+              VisibilityConfig: {
+                SampledRequestsEnabled: true,
+                CloudWatchMetricsEnabled: true,
+                MetricName: `${wafName}-AWSManagedRulesAmazonIpReputationListMetric`,
+              },
+            },
+            {
+              Name: `${wafName}-AWSManagedRulesKnownBadInputsRuleSet`,
+              Priority: 3,
+              OverrideAction: {
+                None: {},
+              },
+              Statement: {
+                ManagedRuleGroupStatement: {
+                  VendorName: "AWS",
+                  Name: "AWSManagedRulesKnownBadInputsRuleSet",
+                },
+              },
+              VisibilityConfig: {
+                SampledRequestsEnabled: true,
+                CloudWatchMetricsEnabled: true,
+                MetricName: `${wafName}-AWSManagedRulesKnownBadInputsRuleSetMetric`,
+              },
+            },
+            {
+              Name: `${wafName}-allow-usa-plus-territories`,
+              Priority: 5,
+              Action: {
+                Allow: {},
+              },
+              Statement: {
+                GeoMatchStatement: {
+                  CountryCodes: ["GU", "PR", "US", "UM", "VI", "MP"],
+                },
+              },
+              VisibilityConfig: {
+                SampledRequestsEnabled: true,
+                CloudWatchMetricsEnabled: true,
+                MetricName: `${wafName}-allow-usa-plus-territories-metric`,
+              },
+            },
+          ],
           Scope: "REGIONAL",
         },
       };
